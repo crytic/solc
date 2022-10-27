c22416deace11aa29d064b279cd22110749ed4e2
bohendo <bohendo@pm.me>
2022-10-27 15:33:12 -0400

    fix refresh script

--- refresh.sh
+++ refresh.sh
@@ -5,17 +5,6 @@ root=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 mapfile -t versions < "$root/versions"
 architectures=( "linux-amd64" "macosx-amd64" "windows-amd64")
 
-function getUrl {
-  arch="$1"
-  version="$2"
-  if [[ -n "$version" ]]
-  # if version was provided, get that binary
-  then echo "https://binaries.soliditylang.org/${arch}/${version}"
-  # else get the list of all available binary versions
-  else echo "https://binaries.soliditylang.org/${arch}/list.json"
-  fi
-}
-
 # Refresh lists of versions available for each architecture
 function refresh_lists {
   for arch in "${architectures[@]}"
@@ -30,7 +19,7 @@ function refresh_lists {
       rm -f "$list.backup"
       mv "$list" "$list.backup"
     fi
-    url="$(getUrl "$arch")"
+    url="https://binaries.soliditylang.org/${arch}/list.json"
     echo "Fetching updated list of available versions for arch $arch from $url"
     if wget -q "$url" -O "$list"
     then rm -f "$list.backup"
@@ -42,6 +31,23 @@ function refresh_lists {
 }
 # refresh_lists
 
+function read_list {
+  arch="$1"
+  version="$2"
+  key="$3"
+  for list in latest legacy
+  do
+    listfile="$root/$arch/list-$list.json"
+    if [[ ! -f "$listfile" ]]
+    then continue # list type doesn't exist for this arch
+    fi
+    if [[ "$(jq '.releases."'"$version"'"' "$listfile")" == "null" ]]
+    then continue # version doesn't exist in this list
+    else jq '.builds[] | select(.version=="'"$version"'" ) | .'"$key" "$listfile" | tr -d '"'
+    fi
+  done
+}
+
 function verify_sha256 {
   arch="$1"
   version="$2"
@@ -52,24 +58,20 @@ function verify_sha256 {
     exit 1
   fi
   actual_sha256="0x$(sha256sum "$target" | cut -d " " -f 1)"
-  for list in latest legacy
-  do
-    listfile="$root/$arch/list-$list.json"
-    long_version=$(jq '.releases."'"$version"'"' "$listfile")
-    # echo "long version of name for version=$version arch=$arch is: $long_version"
-    if [[ "$long_version" == "null" ]]
-    then continue
-    else
-      expected_sha256="$(jq '.builds[] | select(.version=="'"$version"'" ) | .sha256' "$listfile" | tr -d '"')"
-      if [[ "$expected_sha256" != "$actual_sha256" ]]
-      then
-        echo "OH NO, VERY BAD, SHA256 hashes do not match for $target"
-        echo "expected:$expected_sha256 != actual:$actual_sha256"
-        exit 1
-      else return
-      fi
-    fi
-  done
+  expected_sha256="$(read_list "$arch" "$version" "sha256")"
+  if [[ "$expected_sha256" != "$actual_sha256" ]]
+  then
+    echo "OH NO, VERY BAD, SHA256 hashes do not match for $target"
+    echo "expected:$expected_sha256 != actual:$actual_sha256"
+    exit 1
+  fi
+}
+
+function getUrl {
+  arch="$1"
+  version="$2"
+  path="$(read_list "$arch" "$version" "path")"
+  url="https://binaries.soliditylang.org/${arch}/${path}"
 }
 
 # Install any missing solc binaries from the list of supported versions
@@ -79,77 +81,18 @@ do
   do
     list="$root/$arch/list.json"
     target="$root/$arch/solc-v$version"
+    # echo "Checking $target"
     if [[ -f "$target" ]]
     then
       verify_sha256 "$arch" "$version"
       echo "$target has a valid sha256 hash"
     else
       echo "solc-v$version for $arch is missing, attempting to download it now.."
-      wget -q "$(getUrl "$arch" "$version")" -O "$target"
+      path="$(read_list "$arch" "$version" "path")"
+      url="https://binaries.soliditylang.org/${arch}/${path}"
+      wget -q "$url" -O "$target"
       verify_sha256 "$arch" "$version"
       echo "solc-v$version has been downloaded & its sha256 hash has been validated"
     fi
   done
 done
-
-# 
-# 
-# function getDigest {
-#   distro="$1"
-#   curl -Ls "$(getUrl "$distro").sha256" | cut -d " " -f 1
-# }
-# 
-# bin="$root/bin"
-# cache="$root/.cache"
-# tmp="$root/.tmp"
-# mkdir -p "$bin" "$cache" "$tmp"
-# 
-# ( cd "$tmp" || exit
-# 
-#   for distro in "${distros[@]}";
-#   do
-# 
-#     # Download tarballs for each distro (unless cached)
-#     outfile="echidna-${version}-${distro}.tar.gz"
-#     if [[ ! -f "$cache/$outfile" ]]
-#     then
-#       echo "No tarball exists at $cache/$outfile"
-#       url="$(getUrl "$distro")"
-#       echo "Fetching echidna v${version} for ${distro}"
-#       wget "$url" --output-document "$outfile"
-#       digest="$(getDigest "$distro")"
-#       echo "expected sha256: $digest"
-#       received_digest=$(sha256sum "$outfile" | cut -d " " -f 1)
-#       if [[ "$digest" != "$received_digest" ]]
-#       then echo "Error, expected digest != received digest: ${digest} != $received_digest" && exit 1
-#       fi
-#       echo "Checksum matches, saving this tarball to a local cache"
-#       mv "$outfile" "$cache"
-#     fi
-# 
-#     # Unpack tarballs for each distro (unless bins are present)
-#     cd "$bin" || exit
-#     dir="echidna/${version}/${distro}"
-#     mkdir -p "$dir"
-#     cd "$dir" || exit
-#     if [[ -f "echidna-test" ]]
-#     then
-#       echo "Echidna $version binary for $distro already exists"
-#     else
-#       tarball="$cache/$outfile"
-#       echo "Unpacking contents of $tarball to $(pwd)"
-#       tar -xzf "$tarball"
-#       if [[ -d "echidna-test" ]]
-#       then
-#         mv -f echidna-test echidna-dir
-#         mv -f echidna-dir/* .
-#         rmdir echidna-dir
-#       fi
-#     fi
-# 
-#     cd "$tmp" || exit # reset cwd before the next loop
-#   done
-# 
-# )
-# 
-# rm -rf "$tmp"
